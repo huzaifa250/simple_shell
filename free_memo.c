a1eb41eba8895aca0e30da71c3c75431ec832acf
huzaifa250 <huz.dark1@gmail.com>
2023-11-15 14:50:43 -0800

    memory

---
+++ free_memo.c
@@ -0,0 +1,20 @@
+
+#include "shell.h"
+
+/**
+ * bfree - frees a pointer and make address NULL
+ * @ptr: address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+int bfree(void **ptr)
+{
+	if (ptr && *ptr != NULL)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
+
