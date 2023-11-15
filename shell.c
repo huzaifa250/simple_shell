362c8053d7707036c1fd8665862a9413f6e6e3de
huzaifa250 <huz.dark1@gmail.com>
2023-11-14 18:52:40 -0800

    add main shell

---
+++ shell.c
@@ -0,0 +1,119 @@
+#include "shell.h"
+
+/**
+ * hash - main shell loop
+ * @info: the parameter & return info struct
+ * @av: the argument vector from main()
+ *
+ * Return: 0 on success, 1 on error, or error code
+ */
+int hash(info_st *info, char **av)
+{
+	ssize_t re = 0;
+	int builtin_ret = 0;
+
+	while (1)
+	{
+		dell_info(info);
+		if (prompt(info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		re = get_input(info);
+		if (re != -1)
+		{
+			info_set(info, av);
+			builtin_ret = f_builtin(info);
+			if (builtin_ret == -1)
+				f_cmd(info);
+		}
+		else if (prompt(info))
+			_putchar('\n');
+		info_free(info, 0);
+	}
+	info_free(info, 1);
+	if (!prompt(info) && info->status)
+		exit(info->status);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
+}
+
+/**
+ * f_builtin - finds a builtin command
+ * @info: the parameter & return info struct
+ *
+ * Return: -1 if builtin not found,
+ *			0 if builtin executed successfully,
+ *			1 if builtin found but not successful,
+ *			-2 if builtin signals exit()
+ */
+int f_builtin(info_st *info)
+{
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", special_exit},
+		/*{"env", my_env},*/
+		{"help", my_help},
+		{"setenv", set_env},
+		{"unsetenv", unset_env},
+		{"cd", sp_cd},
+		/*{"alias", _myalias},*/
+		{NULL, NULL}
+	};
+
+	for (i = 0; builtintbl[i].type; i++)
+		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+		{
+			info->line_count++;
+			built_in_ret = builtintbl[i].func(info);
+			break;
+		}
+	return (built_in_ret);
+}
+
+/**
+ * f_cmd - finds a command in PATH
+ * @info: the parameter & return info struct
+ *
+ * Return: Nothing
+ */
+void f_cmd(info_st *info)
+{
+	char *path = NULL;
+	int i, j;
+
+	info->path = info->argv[0];
+	if (info->linecount_flag == 1)
+	{
+		info->line_count++;
+		info->linecount_flag = 0;
+	}
+	for (i = 0, j = 0; info->arg[i]; i++)
+		if (!is_delim(info->arg[i], " \t\n"))
+			j++;
+	if (!j)
+		return;
+
+	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		fork_cmd(info);
+	}
+	else
+	{
+		if ((prompt(info) || _getenv(info, "PATH=")
+			|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+			fork_cmd(info);
+		else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			print_error(info, "not found\n");
+		}
+	}
+}
+
