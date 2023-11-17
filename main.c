d2f1b6812c859ade1a395fb0d42e6041b610279b
saharrrrr <skmao23@gmail.com>
2023-11-16 17:30:05 -0800

    updated

--- main.c
+++ main.c
@@ -6,30 +6,31 @@
  */
 int main(void)
 {
-    char *buffer = NULL; /*storage size */
-    char **av = NULL; /*arr of str or command */
-    ssize_t ex;
-    /*int exit_status = 0;*/
+	char *buffer = NULL; /*storage size */
+	char **av = NULL; /*arr of str or command */
+	ssize_t ex; /*int exit_status = 0;*/
 
-    while (1)
-    {
-	    prompt();
-      buffer = _read();/*read input enter by us */
-      if (*buffer != '\0')
-      {
-      	  av = tokenize(buffer);
-        	if (av == NULL)/*if tokenize faill */
-        	{
-		  			free(buffer);
-            continue;
-        	}
-          /*fullpathbuffer = _pathbuffer(av, PATH, copy);
-           if (checkbuiltins(av, buffer, exitstatus) == 1)
-              continue;
-              exitstatus = _forkprocess(av, buffer, fullpathbuffer);*/
-       }
-       else
-           free(buffer);
-      }
-      return (0);
+	while (1)
+	{
+		prompt();
+		buffer = _read();/*read input enter by us */
+		if (*buffer != '\0')
+		{
+			av = tokenize(buffer);
+			if (av == NULL)/*if tokenize faill */
+			{
+				free(buffer);
+				continue;
+			}
+			/**
+			 * fullpathbuffer = _pathbuffer(av, PATH, copy);
+			 * if (checkbuiltins(av, buffer, exitstatus) == 1)
+			 * continue;
+			 * exitstatus = _forkprocess(av, buffer, fullpathbuffer);
+			 **/
+		}
+		else
+			free(buffer);
+	}
+	return (0);
 }
